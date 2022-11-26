3c888ecb44486d93bc3769f84a4c271b72f6819d
Pongpol <trinitymirrorswitch@gmail.com>
2022-11-26 11:21:25 +0700

    Workshop A Week3

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('Hello baboi');
+  res.status(200).json({
+    Fullname:'Kunyanut Techaphalangrak'
+  })
+
+});
+
+router.get('/bio', function(req, res, next) {
+  // res.send('Hello baboi');
+  res.status(200).json({
+    Fullname:'Kunyanut Techaphalangrak',
+    Nickname:"Gun",
+    Hobby:"Drawning",
+    GithubUsername:"TeGun13"
+  })
+
 });
 
 module.exports = router;
